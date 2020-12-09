8fcaa975f0fd9312ba1f58ac4f64c4460edd3196
Ankit Srivastava <ankitsrivas14@gmail.com>
2020-12-10 00:03:23 +0530

    on key up

--- app.js
+++ app.js
@@ -2,12 +2,17 @@ const app = Vue.createApp({
   data() {
     return {
       counter: 0,
-      name: ''
+      name: '',
+      confirmedName: ''
     };
   },
   methods: {
     setName(e){
       this.name = e.target.value;
+    },
+    handleConfirm(){
+      this.confirmedName = this.name;
+      this.name = '';
     }
   }
 });
